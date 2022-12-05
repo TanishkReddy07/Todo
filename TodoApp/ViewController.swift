c1edd79012d0fb442cb308bd1a3bbcc9cbfe93e8
TanishkReddy07 <tanishkreddy512@gmail.com>
2022-12-05 16:42:30 -0500

    Added swipe gesture to cell

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -52,13 +52,39 @@ class ViewController: UIViewController {
     
     // handle navigation to details screen
     func goToDetails(item: Todo?) {
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.type = CATransitionType.moveIn
+        transition.subtype = CATransitionSubtype.fromLeft
+        transition.timingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeInEaseOut)
+        view.window!.layer.add(transition, forKey: kCATransition)
         let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
         let detailsViewController = storyBoard.instantiateViewController(withIdentifier: "TodoDetailsViewController") as! TodoDetailsViewController
         detailsViewController.todo = item
-        self.navigationController?.pushViewController(detailsViewController, animated: true)
+        self.navigationController?.pushViewController(detailsViewController, animated: false)
     }
     
+    //toggle todo complete
+    func toggleTodoComplete(row: Int) {
+        Task.init {
+            let toDo = self.todoList[row]
+                let _ = try await ToDoRepository.shared.updateTodo(name: toDo.name, hasDueDate: toDo.hasDueDate, isCompleted: !toDo.isCompleted, dueDate: toDo.dueDate, notes: toDo.notes, object: toDo)
+            getTodoList()
+            
+            
+        }
+    }
     
+    //delete Todo
+    func deleteTodo(row: Int) {
+        Task.init {
+            let toDo = self.todoList[row]
+                let _ = try await ToDoRepository.shared.deleteTodo(object: toDo)
+            getTodoList()
+            
+            
+        }
+    }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
@@ -76,5 +102,42 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 86
     }
+    
+    // left swipe action
+    func tableView(_ tableView: UITableView,
+                       trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        // left swipe to toggle complete
+        let toggleComplete = UIContextualAction(style: .destructive,
+                                       title: "toggleComplete") { [weak self] (action, view, completionHandler) in
+            self?.toggleTodoComplete(row: indexPath.row)
+            
+            
+        }
+        toggleComplete.backgroundColor = .yellow
+        
+        
+        // left long swipe to delete
+        let longSwipeToDelete = UIContextualAction(style: .destructive,
+                                       title: "Delete") { [weak self] (action, view, completionHandler) in
+            print("Delete")
+            self?.deleteTodo(row: indexPath.row)
+            
+        }
+        longSwipeToDelete.backgroundColor = .red
+        return UISwipeActionsConfiguration(actions: [longSwipeToDelete,toggleComplete])
+    }
+    
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let details = UIContextualAction(style: .destructive,
+                                       title: "Details") { [weak self] (action, view, completionHandler) in
+            self?.goToDetails(item: self?.todoList[indexPath.row])
+            completionHandler(true)
+        }
+        details.backgroundColor = .blue
+        return UISwipeActionsConfiguration(actions: [details])
+    }
+    
+    
 }
 
